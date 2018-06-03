46b7df72bf88d37dadbbb3a29c037222686fa8a1
Jessy974 <39767813+Jessy974@users.noreply.github.com>
2018-06-03 13:57:07 +0200

    class Article

---
+++ Gestion de stocks/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace GestionDeStocks
+{
+    class Article
+    {
+        public uint numeroDeReference { get; set; }
+        public string nom { get; set; }
+        public float prixDeVente { get; set; }
+        public byte quantiteEnStock { get; set; }
+
+        // définition du constructeur
+        public Article (uint numeroDeReference, string nom, float prixDeVente, byte quantiteEnStock)
+            
+        {
+            this.numeroDeReference = numeroDeReference;
+            this.nom = nom;
+            this.prixDeVente = prixDeVente;
+            this.quantiteEnStock = quantiteEnStock;
+
+        }
+
+    }
+}
---
+++ Gestion de stocks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gestion_de_stocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
